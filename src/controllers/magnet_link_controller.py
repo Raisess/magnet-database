f6f3cf631a69266ea403cf2e5dbc21a80155f602
Raisess <raisechif@gmail.com>
2022-12-06 18:22:54 -0300

    controllers/magnet_link: add methods return types

--- src/controllers/magnet_link_controller.py
+++ src/controllers/magnet_link_controller.py
@@ -1,6 +1,7 @@
 from flask import request
 from app.magnet_link.create_magnet_link import CreateMagnetLink
 from app.magnet_link.search_magnet_links import SearchMagnetLinks
+from model.magnet_link_model import MagnetLinkModel
 
 class MagnetLinkController:
   def __init__(
@@ -11,11 +12,11 @@ class MagnetLinkController:
     self._create_magnet_link = create_magnet_link
     self._search_magnet_links = search_magnet_links
 
-  def create_magnet_link(self):
+  def create_magnet_link(self) -> None:
     name = request.args.get("name")
     uri = request.args.get("uri")
     return self._create_magnet_link.handle(name, uri, "temp")
 
-  def search_magnet_links(self):
+  def search_magnet_links(self) -> list[MagnetLinkModel]:
     name = request.args.get("name")
     return self._search_magnet_links.handle(name)
